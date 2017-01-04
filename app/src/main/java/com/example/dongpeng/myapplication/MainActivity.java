2fd60d7a535568ec36f1093aad62baf3f090cca2
dongpeng <457055340@163.com>
2017-01-04 13:44:34 +0800

    仿话费充值界面

--- app/src/main/java/com/example/dongpeng/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dongpeng/myapplication/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SimpleItemAnimator;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,22 +15,23 @@ import java.util.List;
 
 public class MainActivity extends Activity {
     private TextView tv_money;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recharge_lay);
-        tv_money= (TextView) findViewById(R.id.text);
-        RecyclerView rv= (RecyclerView) findViewById(R.id.rv);
+        tv_money = (TextView) findViewById(R.id.text);
+        RecyclerView rv = (RecyclerView) findViewById(R.id.rv);
         //防止recycleview闪烁
-        ((SimpleItemAnimator)rv.getItemAnimator()).setSupportsChangeAnimations(false);
-        GridLayoutManager manager=new GridLayoutManager(this,3);
-        List<MoneyEntity> data=new ArrayList<MoneyEntity>();
-        data.add(new MoneyEntity("5元",false));
-        data.add(new MoneyEntity("10元",false));
-        data.add(new MoneyEntity("30元",false));
-        data.add(new MoneyEntity("50元",false));
-        data.add(new MoneyEntity("100元",false));
-        MyAdapter adapter=new MyAdapter(data,this);
+        ((SimpleItemAnimator) rv.getItemAnimator()).setSupportsChangeAnimations(false);
+        GridLayoutManager manager = new GridLayoutManager(this, 3);
+        List<MoneyEntity> data = new ArrayList<MoneyEntity>();
+        data.add(new MoneyEntity("5元", false));
+        data.add(new MoneyEntity("10元", false));
+        data.add(new MoneyEntity("30元", false));
+        data.add(new MoneyEntity("50元", false));
+        data.add(new MoneyEntity("100元", false));
+        MyAdapter adapter = new MyAdapter(data, this);
         adapter.setMoneyInputListener(new MoneyInputListener() {
             @Override
             public void onGetMoneyInput(String money) {
